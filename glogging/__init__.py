07c643e40f13a51f47405d2714dae284fda507ea
Sam Stephens <sam@growthintel.com>
2017-05-12 16:21:56 +0100

    Fixed glogging import

--- glogging/__init__.py
+++ glogging/__init__.py
@@ -1,3 +1,3 @@
 """We provide the GLogging class and getLoggerFromPath helper
 at the top level so users don't have to `from glogging.glogging import X`"""
-from glogging import GLogging, getLoggerFromPath
+from glogging.glogging import GLogging, getLoggerFromPath
