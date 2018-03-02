38ea05032721e87a299834585bc8aafa77050b6b
Prash <prash@pelucid.com>
2018-03-02 13:48:49 +0000

    bump version and install psutil

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
 setup(name='glogging',
-      version='0.1.1',
+      version='0.2',
       description='GI logger',
       url='http://github.com/pelucid/glogging',
-      packages=['glogging'])
+      packages=['glogging'],
+      install_requires['psutil==5.4.3'])
