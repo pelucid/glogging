746c00755482d673f075637f40d428a5697f3bf8
Prash <prash@pelucid.com>
2018-03-04 19:53:05 +0000

    add docs

--- glogging/resource_metrics.py
+++ glogging/resource_metrics.py
@@ -3,6 +3,12 @@ import psutil
 import os
 
 class ResourceMetricsFilter(logging.Filter):
+    """Implement a resouce usage logging as a filter as per cookbook docs:
+    https://docs.python.org/2/howto/logging-cookbook.html#using-filters-to-impart-contextual-information
+
+    This adds Server Resource Usage Metrics to the LogRecord.
+    """
+
     def __init__(self, *args, **kwargs):
         super(ResourceMetricsFilter, self).__init__(*args, **kwargs)
         self.pid = os.getpid()
