2bc837d63762b9f09db6ac42f21b5f91bedd0ce6
Ryan Varley <ryan@growthintel.com>
2018-03-06 13:46:15 +0000

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='glogging',
-      version='0.2',
+      version='0.3',
       description='GI logger',
       url='http://github.com/pelucid/glogging',
       packages=['glogging'],
