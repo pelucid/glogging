b80684668cba0df40829b665834f85dc5ade1501
Ryan Varley <ryan@growthintel.com>
2018-03-06 14:16:14 +0000

    hotfix for importing decorators

--- glogging/__init__.py
+++ glogging/__init__.py
@@ -2,4 +2,4 @@
 at the top level so users don't have to `from glogging.glogging import X`"""
 from __future__ import absolute_import
 from glogging.glogging import GLogging, getLoggerFromPath
-import decorators
+from glogging import decorators
