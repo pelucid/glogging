eb729642c661aad4c1d1df0f8dcaf9a8673a199b
Max Awan <max.awan@digital.justice.gov.uk>
2019-05-02 12:10:19 +0100

    python 3 porting

--- glogging/__init__.py
+++ glogging/__init__.py
@@ -1,5 +1,5 @@
 """We provide the GLogging class and getLoggerFromPath helper
 at the top level so users don't have to `from glogging.glogging import X`"""
 
-from glogging.glogging import GLogging, getLoggerFromPath
-from glogging import decorators
+from .glogging import GLogging, getLoggerFromPath
+from . import decorators
