51229e7cc049002784a0f198774f9fa270df29e8
andrew-grwothintel <andrew@growthintellligence@.com>
2021-12-29 12:16:29 +0000

    Added makefile, travis file and split out Python 2 and 3 dependencies

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+import sys
 from setuptools import setup
+
+requirements = []
+
+py3_requirements = [
+    'psutil==5.6.6'
+]
+
+if sys.version_info[0] == 2:
+    with open('py2requirements.txt') as f:
+        py2_requirements = f.read().splitlines()
+    requirements += py2_requirements
+else:
+    requirements += py3_requirements
 
 setup(name='glogging',
       version='0.5',
       description='GI logger',
       url='http://github.com/pelucid/glogging',
       packages=['glogging'],
-      install_requires=['psutil==5.6.6'])
+      install_requires=requirements)
